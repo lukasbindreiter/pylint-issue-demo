28c188ca4e8ded86c5bd15fbd93b310e72e5b14f
Lukas Bindreiter <lukas.bindreiter@cloudflight.io>
2022-06-29 15:54:07 +0200

    Remove functional example

--- functional-u/setup.py
+++
@@ -1,7 +0,0 @@
-import setuptools
-
-setuptools.setup(
-    name="functional-u",
-    version="0.0.1",
-    packages=setuptools.find_namespace_packages(include=["functional.*"]),
-)
--- functional-w/setup.py
+++
@@ -1,7 +0,0 @@
-import setuptools
-
-setuptools.setup(
-    name="functional-w",
-    version="0.0.1",
-    packages=setuptools.find_namespace_packages(include=["functional.*"]),
-)
